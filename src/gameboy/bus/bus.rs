4161d51a4eb56ff5f80b92ffd651957620f644a0
Thomas <thomas@thomasw.dev>
2023-02-28 21:04:50 +0100

    Implement write_slice method for Bus

--- src/gameboy/bus/bus.rs
+++ src/gameboy/bus/bus.rs
@@ -1,4 +1,10 @@
 pub trait Bus {
     fn read(&self, addr: u16) -> u8;
     fn write(&mut self, addr: u16, val: u8);
+
+    fn write_slice(&mut self, from: &[u8], offset: usize) {
+        for (i, b) in from.into_iter().enumerate() {
+            self.write((offset + i) as u16, *b);
+        }
+    }
 }
