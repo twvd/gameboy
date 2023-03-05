2e0f9892ba14123344ad8b7bdcbaaa422c438799
Thomas <thomas@thomasw.dev>
2023-03-05 07:45:56 +0100

    Make pausing on each step in 'dbg' optional

--- src/bin/dbg/main.rs
+++ src/bin/dbg/main.rs
@@ -1,21 +1,30 @@
-use std::env;
 use std::fs;
 use std::io::{stdin, Read};
 
-use anyhow::{bail, Result};
+use anyhow::Result;
+use clap::Parser;
 
 use gbrust::gameboy::bus::bus::Bus;
 use gbrust::gameboy::bus::testbus::Testbus;
 use gbrust::gameboy::cpu::cpu::CPU;
 
+#[derive(Parser)]
+#[command(
+    about = "A crude ROM debugger for development purposes",
+    long_about = None)]
+struct Args {
+    /// ROM filename to load.
+    filename: String,
+
+    /// Wait for keystroke after each CPU step.
+    #[arg(short, long)]
+    pause: bool,
+}
+
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        bail!("Syntax: {} <filename>", args[0]);
-    }
-
-    let f = fs::read(&args[1])?;
+    let f = fs::read(args.filename)?;
     let mut bus = Testbus::new();
     bus.write_slice(&f, 0);
     let mut cpu = CPU::new(Box::from(bus));
@@ -25,7 +34,9 @@ fn main() -> Result<()> {
         println!("{}", cpu.regs);
         println!(" --> {}", cpu.peek_next_instr()?);
 
-        let _ = stdin().read(&mut [0u8]).unwrap();
+        if args.pause {
+            let _ = stdin().read(&mut [0u8]).unwrap();
+        }
 
         cpu.step()?;
     }
