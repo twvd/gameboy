twvd/gameboy

commit 05f0c0ab4f96ab51d37cf7e6c4149ca59059f9b4

Pass RGB555 colors from LCD controller to display output
Before, we passed the (DMG) 2-bit greyscale values from
the LCD controller to the display and the display did the
translation to output colors.

Now, the LCD controller does the translation when in
DMG mode (for now: always) and passes RGB555 colors to
the display output, so when later the CGB LCD features
are implemented, we can actually display colors.

Truecolor on the terminal is actually quite slow, so
for now, translate the colors to the 256-color ANSI
palette for the terminal output driver. The Sixel
driver supports the full 15-bit palette.

Truecolor for terminal will be added later.

src/gameboy/lcd.rs
@@ -9,6 +9,9 @@ use num_traits::ToPrimitive;
 pub const LCD_W: usize = 160;
 pub const LCD_H: usize = 144;
 
+/// Type of a color definition (RGB555)
+pub type Color = u16;
+
 const VRAM_SIZE: usize = 0x2000;
 
 // Tile sizes
@@ -354,14 +357,20 @@ impl LCDController {
     /// Converts a color index to a color from the
     /// BG/OBJ palettes
     /// (DMG-mode only)
-    fn palette_convert(cidx: u8, palette: u8) -> u8 {
-        (palette >> (cidx * 2)) & 3
+    fn palette_convert_dmg(cidx: u8, palette: u8) -> Color {
+        match (palette >> (cidx * 2)) & 3 {
+            3 => 0,
+            2 => 0b01000_01000_01000,
+            1 => 0b11000_11000_11000,
+            0 => 0b11111_11111_11111,
+            _ => unreachable!(),
+        }
     }
 
     fn draw_tile_at(
         &self,
         tile: &[u8],
-        line: &mut [u8],
+        line: &mut [Color],
         x: isize,
         y: isize,
         palette: u8,
@@ -424,7 +433,7 @@ impl LCDController {
                     }
                 }
 
-                let color = Self::palette_convert(color_idx, palette);
+                let color = Self::palette_convert_dmg(color_idx, palette);
 
                 line[disp_x as usize] = color;
             }

src/test/acid.rs
@@ -6,7 +6,7 @@ use hex_literal::hex;
 fn dmg_acid2() {
     test_display(
         include_bytes!("../../tests/dmg-acid2/dmg-acid2.gb"),
-        &hex!("3b4e785d4de53352e90bf6f51eea85600a0b57c410b9f48d1f2ded84cfacf513"),
+        &hex!("117528fd9c5efb1feaec71c57637db9e7370fae07bf5e419b44e5f044aa86774"),
         20000,
     );
 }

src/display/display.rs
@@ -1,5 +1,5 @@
-/// Type of a color definition.
-pub type Color = u32;
+/// Type of a color definition (RGB555)
+pub type Color = u16;
 
 /// Base trait for a display output
 pub trait Display: std::any::Any {

src/display/sixel.rs
@@ -28,9 +28,9 @@ impl SixelDisplay {
         Self {
             width,
             height,
-            depth: 3,
+            depth: 2,
             scale: 4,
-            buffer: vec![0; width * height * 3 * 4 * 4],
+            buffer: vec![0; width * height * 2 * 4 * 4],
             encoder,
             updates: 0,
             last_frame: Instant::now(),
@@ -53,22 +53,12 @@ impl SixelDisplay {
 
 impl Display for SixelDisplay {
     fn set_pixel(&mut self, x: usize, y: usize, color: Color) {
-        let (r, g, b) = match color {
-            3 => (0, 0, 0),
-            2 => (90, 90, 90),
-            1 => (160, 160, 160),
-            0 => (255, 255, 255),
-            _ => {
-                println!("{}", color);
-                unreachable!()
-            }
-        };
-
         for px in (x * self.scale)..((x + 1) * self.scale) {
             for py in (y * self.scale)..((y + 1) * self.scale) {
-                self.buffer[px * self.depth + py * self.depth * self.width * self.scale + 0] = r;
-                self.buffer[px * self.depth + py * self.depth * self.width * self.scale + 1] = g;
-                self.buffer[px * self.depth + py * self.depth * self.width * self.scale + 2] = b;
+                self.buffer[px * self.depth + py * self.depth * self.width * self.scale + 0] =
+                    (color >> 8) as u8;
+                self.buffer[px * self.depth + py * self.depth * self.width * self.scale + 1] =
+                    (color & 0xFF) as u8;
             }
         }
     }
@@ -85,7 +75,7 @@ impl Display for SixelDisplay {
             let frame = QuickFrameBuilder::new()
                 .width(self.width * self.scale)
                 .height(self.height * self.scale)
-                .format(PixelFormat::RGB888)
+                .format(PixelFormat::RGB555)
                 .pixels(self.buffer.clone());
 
             self.move_cursor(0, 0);

src/display/terminal.rs
@@ -24,7 +24,40 @@ pub struct TerminalDisplay {
 }
 
 /// Flag to mark a pixel for redrawing
-const DISP_DIRTY: u32 = 1 << 31;
+/// We use bit 15 as RGB555 only uses bits 0 - 14.
+const DISP_DIRTY: u16 = 1 << 15;
+
+fn unpack_rgb555(c: Color) -> (u8, u8, u8) {
+    (
+        ((c >> 10) & 0x1F) as u8,
+        ((c >> 5) & 0x1F) as u8,
+        (c & 0x1F) as u8,
+    )
+}
+
+fn rgb555_to_rgb888((r, g, b): (u8, u8, u8)) -> (u8, u8, u8) {
+    (
+        (r as u16 * 255 / 31) as u8,
+        (g as u16 * 255 / 31) as u8,
+        (b as u16 * 255 / 31) as u8,
+    )
+}
+
+fn rgb888_to_ansi((r, g, b): (u8, u8, u8)) -> u8 {
+    if r == g && g == b {
+        if r < 8 {
+            return 0;
+        }
+
+        if r > 248 {
+            return 231;
+        }
+
+        return (((r as u16 - 8) * 24) / 247) as u8 + 232;
+    }
+
+    16 + (36 * (r / 255 * 5)) + (6 * (g / 255 * 5)) + (b / 255 * 5)
+}
 
 impl TerminalDisplay {
     pub fn new(width: usize, height: usize, fps: u64) -> Self {
@@ -38,6 +71,7 @@ impl TerminalDisplay {
         }
 
         let term = terminal::stdout();
+        term.act(Action::ResetColor).unwrap();
         term.act(Action::HideCursor).unwrap();
         term.act(Action::DisableBlinking).unwrap();
         term.act(Action::ClearTerminal(Clear::All)).unwrap();
@@ -58,15 +92,13 @@ impl TerminalDisplay {
         TerminalInput::new(terminal::stdout())
     }
 
-    /// Map a color from.our internal color type to a terminal color
+    /// Map a color from our internal color type to a terminal color
     fn map_color(&self, c: Color) -> TerminalColor {
-        match c {
-            3 => TerminalColor::AnsiValue(232),
-            2 => TerminalColor::AnsiValue(240),
-            1 => TerminalColor::AnsiValue(248),
-            0 => TerminalColor::AnsiValue(255),
-            _ => unreachable!(),
+        let ansi = rgb888_to_ansi(rgb555_to_rgb888(unpack_rgb555(c)));
+        if ansi != 16 {
+            //panic!("{}", ansi);
         }
+        TerminalColor::AnsiValue(ansi)
     }
 
     /// Map a pair of two vertically adjacent pixels to a printable
@@ -152,5 +184,42 @@ impl Display for TerminalDisplay {
             sleep(Duration::from_micros(self.frametime - framelen));
         }
         self.last_frame = Instant::now();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rgb555_to_rgb888() {
+        assert_eq!(rgb555_to_rgb888((0, 0, 0)), (0, 0, 0));
+        assert_eq!(rgb555_to_rgb888((0x1F, 0x1F, 0x1F)), (255, 255, 255));
+
+        assert_eq!(rgb555_to_rgb888((0b01000, 0b01000, 0b01000)), (65, 65, 65));
+        assert_eq!(
+            rgb555_to_rgb888((0b10000, 0b10000, 0b10000)),
+            (131, 131, 131)
+        );
+    }
+
+    #[test]
+    fn test_unpack_rgb555() {
+        assert_eq!(unpack_rgb555(0x7FFF), (0x1F, 0x1F, 0x1F));
+        assert_eq!(unpack_rgb555(0), (0, 0, 0));
+        assert_eq!(unpack_rgb555(0b01000_01000_01000), (8, 8, 8));
+        assert_eq!(unpack_rgb555(0b10000_10000_10000), (16, 16, 16));
+
+        assert_eq!(unpack_rgb555(0b11111_00000_00000), (0x1F, 0, 0));
+        assert_eq!(unpack_rgb555(0b00000_11111_00000), (0, 0x1F, 0));
+        assert_eq!(unpack_rgb555(0b00000_00000_11111), (0, 0, 0x1F));
+    }
+
+    #[test]
+    fn test_rgb888_to_ansi() {
+        assert_eq!(rgb888_to_ansi((0, 0, 0)), 0);
+        assert_eq!(rgb888_to_ansi((65, 65, 65)), 237);
+        assert_eq!(rgb888_to_ansi((131, 131, 131)), 243);
+        assert_eq!(rgb888_to_ansi((255, 255, 255)), 231);
     }
 }

commit 2bf7753b151b9fd205907d65c38ac2d3113dbf35

Fix regression in object priority rendering
This was introduced in the color passing change.
The dmg-acid2 shows this, but the hash was updated in
the change assuming the hash change was just because
of the color changes.

It was revealed by viewing the dmg-acid2 test manually.

Fixes: 05f0c0a

src/gameboy/lcd.rs
@@ -62,6 +62,7 @@ const CRAM_ENTRIES: usize = 0x20;
 const XCPS_ADDR_MASK: u8 = 0x3F;
 const XCPS_AUTO_INC: u8 = 1 << 7;
 const COLOR_MASK: Color = 0x7FFF;
+const COLOR_DEFAULT: Color = 0x7FFF; // White
 const CGB_PALETTE_SIZE: usize = 4;
 
 // BG map attributes (VRAM bank 1, CGB only)
@@ -506,7 +507,7 @@ impl LCDController {
             3 => 0,
             2 => 0b01000_01000_01000,
             1 => 0b11000_11000_11000,
-            0 => 0b11111_11111_11111,
+            0 => COLOR_DEFAULT,
             _ => unreachable!(),
         }
     }
@@ -576,12 +577,12 @@ impl LCDController {
                     // BG priority
                     if !self.cgb {
                         if oflags & OAM_BGW_PRIORITY == OAM_BGW_PRIORITY
-                            && line[disp_x as usize] > 0
+                            && line[disp_x as usize] != COLOR_DEFAULT
                         {
                             continue;
                         }
                     } else {
-                        if line[disp_x as usize] > 0
+                        if line[disp_x as usize] != COLOR_DEFAULT
                             && (self.lcdc & LCDC_CGB_BGW_MASTER_PRIORITY
                                 == LCDC_CGB_BGW_MASTER_PRIORITY)
                             && (oflags & OAM_BGW_PRIORITY == OAM_BGW_PRIORITY)
@@ -606,7 +607,7 @@ impl LCDController {
             return;
         }
 
-        let mut line = [0; LCD_W];
+        let mut line = [COLOR_DEFAULT; LCD_W];
 
         // Background
         if self.cgb || self.lcdc & LCDC_BGW_ENABLE == LCDC_BGW_ENABLE {

src/test/acid.rs
@@ -6,7 +6,7 @@ use hex_literal::hex;
 fn dmg_acid2() {
     test_display(
         include_bytes!("../../tests/dmg-acid2/dmg-acid2.gb"),
-        &hex!("117528fd9c5efb1feaec71c57637db9e7370fae07bf5e419b44e5f044aa86774"),
+        &hex!("d6b6323524d570d90f34793530f51a026cdfeaf1103b674d0c88be87f44ab92e"),
         20000,
     );
 }