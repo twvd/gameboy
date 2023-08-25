e6523400e2afec9cbc51797fc72472d2ffa49c20
Thomas <thomas@thomasw.dev>
2023-08-25 06:34:46 +0200

    Fix crash when reading external RAM for the ROMOnly cart

--- src/gameboy/cartridge/romonly.rs
+++ src/gameboy/cartridge/romonly.rs
@@ -6,9 +6,11 @@ pub struct RomOnly {
 }
 
 impl RomOnly {
+    const ROM_SIZE: usize = 32 * 1024;
+
     pub fn new(rom: &[u8]) -> Self {
         let mut cart = Self {
-            rom: [0; 32 * 1024],
+            rom: [0; Self::ROM_SIZE],
         };
         cart.rom.copy_from_slice(rom);
         cart
@@ -23,7 +25,11 @@ impl Cartridge for RomOnly {
 
 impl BusMember for RomOnly {
     fn read(&self, addr: u16) -> u8 {
-        self.rom[addr as usize]
+        if addr as usize > self.rom.len() {
+            0xFF
+        } else {
+            self.rom[addr as usize]
+        }
     }
 
     fn write(&mut self, _addr: u16, _val: u8) {}
